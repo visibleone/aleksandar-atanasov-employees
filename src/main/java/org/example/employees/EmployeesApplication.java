66bd61d1c3567780007f853b57fd968ddfef6475
Alex <visibleone@gmail.com>
2025-06-17 14:32:09 +0300

    chore: Init project

---
+++ src/main/java/org/example/employees/EmployeesApplication.java
@@ -0,0 +1,12 @@
+package org.example.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeesApplication.class, args);
+  }
+}
---
+++ src/test/java/org/example/employees/EmployeesApplicationTests.java
@@ -0,0 +1,11 @@
+package org.example.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
